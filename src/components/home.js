04ba8b0840379b10018cb7cf12f8e7871bdab579
Pasley <nmp0912@live.missouristate.edu>
2020-02-21 15:58:40 -0600

    Added Link tags to photos to route to the details page

--- src/components/home.js
+++ src/components/home.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => (
     <div className="main container-fluid">
         <div className="page-header">
             <h1>Photo Gallery</h1>
         </div>
-            <a href="/details"><img src={require('../images/Lizard.jpg')} className="photo" alt="lizard"></img></a>
-            <a href="/details"><img src={require('../images/gumballs.jpg')} className="photo" alt="gumball"></img></a>
-            <a href="/details"><img src={require('../images/penquins03.jpg')} className="photo" alt="penquin03"></img></a>
-            <a href="/details"><img src={require('../images/parrot.jpg')} className="photo" alt="parrot"></img></a>
-            <a href="/details"><img src={require('../images/Greece_Street.jpg')} className="photo" alt="greece"></img></a>
+            <Link to="/details"><img src={require('../images/Lizard.jpg')} className="photo" alt="lizard"></img></Link>
+            <Link to="/details"><img src={require('../images/gumballs.jpg')} className="photo" alt="gumball"></img></Link>
+            <Link to="/details"><img src={require('../images/penquins03.jpg')} className="photo" alt="penquin03"></img></Link>
+            <Link to="/details"><img src={require('../images/parrot.jpg')} className="photo" alt="parrot"></img></Link>
+            <Link to="/details"><img src={require('../images/Greece_Street.jpg')} className="photo" alt="greece"></img></Link>
     </div>
 
 );
