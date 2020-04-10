23a7e9c61946151e39f18f388fc9c8d074af6967
Nathan Pasley <37910323+nathanpasley@users.noreply.github.com>
2020-04-10 12:28:47 -0500

    created new branch from last checkpoint

--- src/components/penquins.js
+++ src/components/penquins.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Penquin = () => (
     <div className="main container-fluid">
         <div className="page-header">
             <h1>Penquins</h1>
         </div>
-            <Link to="/details"><img src={require('../images/penquins01.jpg')} className="photo" alt="penquin01"></img></Link>
-            <Link to="/details"><img src={require('../images/penquins02.jpeg')} className="photo" alt="penquin02"></img></Link>
-            <Link to="/details"><img src={require('../images/penquins03.jpg')} className="photo" alt="penquin03"></img></Link>
-            <Link to="/details"><img src={require('../images/penquins04.jpg')} className="photo" alt="penquin04"></img></Link>
+            <a href="/details"><img src={require('../images/penquins01.jpg')} className="photo" alt="penquin01"></img></a>
+            <a href="/details"><img src={require('../images/penquins02.jpeg')} className="photo" alt="penquin02"></img></a>
+            <a href="/details"><img src={require('../images/penquins03.jpg')} className="photo" alt="penquin03"></img></a>
+            <a href="/details"><img src={require('../images/penquins04.jpg')} className="photo" alt="penquin04"></img></a>
     </div>
 );
 
--- src/spanText.js
+++
@@ -1,5 +0,0 @@
-var span = document.querySelector('#text');
-
-export default function spanText() {
-    span.textContent = "Recent Photos";
-}
