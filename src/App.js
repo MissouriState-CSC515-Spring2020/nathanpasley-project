b3d8d1478730b22adfe70b8718c7448465ed4215
Nathan Pasley <37910323+nathanpasley@users.noreply.github.com>
2020-05-01 20:57:29 -0500

    create checkpoint 6

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import {lazy, Suspense} from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min.js';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './index.css';
 import NavBar from './components/navbar';
-import Home from './components/home';
-import Penquin from './components/penquins';
-import Monkey from './components/monkeys';
-import Tiger from './components/tigers';
 
+
+const Home = lazy(() => import('./components/home'));
+const Penquin = lazy(() => import('./components/penquins'));
+const Monkey = lazy(() => import('./components/monkeys'));
+const Tiger = lazy(() => import('./components/tigers'));
 
 
 function App() {
@@ -17,10 +19,12 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <NavBar/>
+        <Suspense fallback="{<>Loading</>}">
         <Route exact path="/" component={Home}/>
         <Route path="/penquins" component={Penquin}/>
         <Route path="/monkeys" component={Monkey}/>
         <Route path="/tigers" component={Tiger}/>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
